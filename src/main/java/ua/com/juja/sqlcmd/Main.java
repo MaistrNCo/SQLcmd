6eea5580d8f11f0ee023b137e2fea77ac2fa164b
MaistrNCo <uncle.claus@gmail.com>
2017-03-07 22:26:41 +0200

    basic operations (insert, update, delete, select )

--- src/main/java/ua/com/juja/sqlcmd/Main.java
+++ src/main/java/ua/com/juja/sqlcmd/Main.java
@@ -1,6 +1,7 @@
 package ua.com.juja.sqlcmd;
 
 import java.sql.*;
+import java.util.Random;
 
 /**
  * Created by maistrenko on 02.03.17.
@@ -23,6 +24,17 @@ public class Main {
                     "postgres");
 
             Statement statement = connection.createStatement();
+
+        //select tables
+            String selectTableList = "SELECT table_name FROM information_schema.tables "+
+                    "WHERE table_schema='public' "+
+                    "AND table_type='BASE TABLE'";
+            ResultSet rset = statement.executeQuery(selectTableList);
+            while (rset.next()) {
+                System.out.println(" "+rset.getString("table_name"));
+            }
+
+
         //delete
             String deleteRowSQL = "delete from users where name like 'baba%' ";
             int numRow = statement.executeUpdate(deleteRowSQL);
@@ -30,6 +42,20 @@ public class Main {
         //insert
             String insertRowSQL = "insert into users (name,password) values ('baba galamaga','157')";
             statement.executeUpdate(insertRowSQL);
+        //update
+
+            String newName = "pass"+ new Random().nextInt();
+            String updateSQL = "update users set password = '"+ newName +"' where id = 3";
+            statement.executeUpdate(updateSQL);
+
+        //update prepared
+            String updateTableSQL = "UPDATE USERS SET NAME = ? WHERE ID = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(updateTableSQL);
+            preparedStatement.setString(1, "helen ");
+            preparedStatement.setInt(2, 3);
+        // execute insert SQL stetement
+            preparedStatement .executeUpdate();
+
         //select
             String selectTableSQL = "SELECT id, name from users";
             ResultSet rs = statement.executeQuery(selectTableSQL);
