8385c12dfb34c9516255d0a25d96214a9dee418a
Mikola Maistrenko <uncle.claus@gmail.com>
2017-05-20 11:38:13 +0300

    new parser in command 'Find' + minor fixes

--- src/main/java/ua/com/juja/maistrenko/sqlcmd/controller/command/Find.java
+++ src/main/java/ua/com/juja/maistrenko/sqlcmd/controller/command/Find.java
@@ -1,5 +1,7 @@
 package ua.com.juja.maistrenko.sqlcmd.controller.command;
 
+import ua.com.juja.maistrenko.sqlcmd.controller.command.parse.ExactAmountParamsParser;
+import ua.com.juja.maistrenko.sqlcmd.controller.command.parse.Parser;
 import ua.com.juja.maistrenko.sqlcmd.model.DBManager;
 import ua.com.juja.maistrenko.sqlcmd.model.RowData;
 import ua.com.juja.maistrenko.sqlcmd.view.View;
@@ -8,6 +10,10 @@ import java.util.List;
 import java.util.Set;
 
 public class Find implements Command {
+    private static final String DESCRIPTION = "find|tableName - to show all data from table 'tableName'";
+    private static final String COMMAND_PATTERN = "find|tableName";
+    private static final int TABLE_NAME_INDEX = 1;
+    private Parser parser = new ExactAmountParamsParser();
     private final View view;
     private final DBManager dbManager;
 
@@ -23,10 +29,21 @@ public class Find implements Command {
 
     @Override
     public void process(String userInput) {
-        String[] params = prepareParams(userInput, 2);
-        String tableName = params[1];
-        Set <String> columnsNames = dbManager.getColumnsNames(tableName);
-        List<RowData> rowDatas = dbManager.selectAllFromTable(tableName);
+        List<String> params = parser.parseInputString(userInput);
+        if (parser.isHelpNeeded(params)) {
+            view.write(DESCRIPTION);
+            return;
+        }
+
+        if (!parser.checkParamsAmount(params,COMMAND_PATTERN)) {
+            view.writeWrongParamsMsg(COMMAND_PATTERN,userInput);
+            return;
+        }
+        dbManager.drop(params.get(TABLE_NAME_INDEX));
+        Set <String> columnsNames = dbManager.getColumnsNames(params.get(TABLE_NAME_INDEX));
+        List<RowData> rowDatas = dbManager.selectAllFromTable(params.get(TABLE_NAME_INDEX));
+
+
         StringBuilder header = new StringBuilder("|");
         for (String colName : columnsNames) {
             header.append(colName + "\t|");
