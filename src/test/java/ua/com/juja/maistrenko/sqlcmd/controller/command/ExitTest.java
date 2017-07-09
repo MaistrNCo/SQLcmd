c42b5d675820cba5a28b06e3b7952301cc1a4020
Mykola Maistrenko <uncle.claus@gmail.com>
2017-07-09 16:39:22 +0300

    added tests for command 'help' refactored tests for 'exit'

--- src/test/java/ua/com/juja/maistrenko/sqlcmd/controller/command/ExitTest.java
+++ src/test/java/ua/com/juja/maistrenko/sqlcmd/controller/command/ExitTest.java
@@ -1,32 +1,40 @@
 package ua.com.juja.maistrenko.sqlcmd.controller.command;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import ua.com.juja.maistrenko.sqlcmd.model.DBManager;
 import ua.com.juja.maistrenko.sqlcmd.view.View;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
 public class ExitTest {
 
-    private final View view = Mockito.mock(View.class);
-    private final DBManager dbManager = Mockito.mock(DBManager.class);
+    private View view;
+    private DBManager dbManager;
+    private Command command;
 
+    @Before
+    public void init() {
+        view = mock(View.class);
+        dbManager = mock(DBManager.class);
+        command = new Exit(dbManager, view);
+    }
     @Test
     public void testExitCanProcessTrue() {
-        Command command = new Exit(dbManager, view);
         assertTrue(command.canProcess("exit"));
     }
 
     @Test
     public void testExitCanProcessFalse() {
-        Command command = new Exit(dbManager, view);
-        assertTrue(!command.canProcess("Exit"));
+        assertFalse(command.canProcess("Exit"));
+        assertFalse(command.canProcess("exit|we"));
     }
 
     @Test
     public void testExitProcess() {
-        Command command = new Exit(dbManager, view);
         try {
             command.process("exit");
         } catch (NormalExitException e) {
